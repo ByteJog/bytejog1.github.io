f7b459c95efbdb6200799865e591cc059e92d148
ByteJog <67696743+ByteJog@users.noreply.github.com>
2020-07-15 22:11:56 +0800

    Update script.js

--- 智能网络摄像头/scripts/script.js
+++ 智能网络摄像头/scripts/script.js
@@ -40,25 +40,6 @@ function enableCam(event) {
   });
 }
 
-
-// Placeholder function for next step.
-function predictWebcam() {
-  // Store the resulting model in the global scope of our app.
-var model = undefined;
-
-// Before we can use COCO-SSD class we must wait for it to finish
-// loading. Machine Learning models can be large and take a moment 
-// to get everything needed to run.
-// Note: cocoSsd is an external object loaded from our index.html
-// script tag import so ignore any warning in Glitch.
-cocoSsd.load().then(function (loadedModel) {
-  model = loadedModel;
-  // Show demo section now model is ready to use.
-  demosSection.classList.remove('invisible');
-});
-}
-
-
 // Pretend model has loaded so we can try out the webcam code.
 var model = true;
 demosSection.classList.remove('invisible');
